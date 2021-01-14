20e75ee249e03510b36f66f948259e7088aca600
AlanContreras-prog <72994436+AlanContreras-prog@users.noreply.github.com>
2021-01-14 01:01:47 -0600

    Update hola.c

--- hola.c
+++ hola.c
@@ -3,15 +3,7 @@
 void saludo(void) //Declarando e Implementando la función
 {
  	printf("Hola Mundo\n"); /* imprime la cadena */
-	return; /* sale de la función */
+	return 0; /* sale de la función */
 }
- 
-int main(void) /* Función principal del programa */
-{
-	saludo(); /* llamada a la función saludo*/
-	saludo(); /* llamada a la función saludo*/
-	saludo(); /* llamada a la función saludo*/
-	saludo(); /* llamada a la función saludo*/
-	return 0; /* sale del programa con código 0 (correcto) */
-}
+
 
