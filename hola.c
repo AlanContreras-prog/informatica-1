7c1a81994e45ca45c09d2bfb631087979d12690f
alan <you@example.com>
2021-01-14 01:23:18 -0600

    new

--- hola.c
+++ hola.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 					
-void saludo(void) //Declarando e Implementando la función
+int mai () //Declarando e Implementando la función
 {
  	printf("Hola Mundo\n"); /* imprime la cadena */
 	return 0; /* sale de la función */
